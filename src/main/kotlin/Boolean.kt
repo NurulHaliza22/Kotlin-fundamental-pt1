2c9e77f3aa235888aadf8fb1b80b6cc8d5ee4b9f
NurulHaliza22 <69024052+NurulHaliza22@users.noreply.github.com>
2020-08-12 21:51:26 +0700

    Add files via upload

---
+++ src/main/kotlin/Boolean.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val nilai = 80
+    val hasil : String
+    hasil = if (nilai <=90 && nilai >80){
+        "A"
+    }
+    else if (nilai <=80 && nilai >70){
+        "B"
+    }
+    else if (nilai <=70 && nilai >60){
+        "C"
+    }
+    else if (nilai <=60 && nilai >50){
+        "D"
+    }
+    else {
+        "E"
+    }
+    print(hasil)
+}
